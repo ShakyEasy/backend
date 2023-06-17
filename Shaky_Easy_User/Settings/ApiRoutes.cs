c38f74c12545759cb8406872feda8a2cca5211b3
Farid <faridbusiness@bk.ru>
2023-06-18 00:27:08 +0200

    changed accesibility level and corrected the class name

--- Shaky_Easy_User/Settings/ApiRoutes.cs
+++ Shaky_Easy_User/Settings/ApiRoutes.cs
@@ -2,18 +2,18 @@
 {
     public static class ApiRoutes
     {
-        public const string _root = "api";
+        private const string _root = "api";
 
-        public const string _version = "";
+        private const string _version = "v1";
 
-        public const string _base = _root + "/" + _version;
+        private const string _base = _root + "/" + _version;
 
 
-        public static class Proofile
+        public static class Profile
         {
             public const string GetAll = _base + "/profiles";
 
-            public const string Get = _base + "/profile/{id}";
+            public const string Get = _base + "/profiles/{id}";
 
             //public const string Post = _base + "/profile/{id}";
 
