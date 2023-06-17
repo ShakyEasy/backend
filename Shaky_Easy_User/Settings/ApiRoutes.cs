04c5c8dbd337af13cbb517740b451239b12c518a
Farid <faridbusiness@bk.ru>
2023-06-17 23:34:05 +0200

    added routing endpoint names

---
+++ Shaky_Easy_User/Settings/ApiRoutes.cs
@@ -0,0 +1,22 @@
+﻿namespace Shaky_Easy_User.Settings
+{
+    public static class ApiRoutes
+    {
+        public const string _root = "api";
+
+        public const string _version = "";
+
+        public const string _base = _root + "/" + _version;
+
+
+        public static class Proofile
+        {
+            public const string GetAll = _base + "/profiles";
+
+            public const string Get = _base + "/profile/{id}";
+
+            //public const string Post = _base + "/profile/{id}";
+
+        }
+    }
+}
