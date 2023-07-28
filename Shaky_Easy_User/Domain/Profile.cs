761748e5de756ab8af6219e5ec7b094899766145
Farid <faridbusiness@bk.ru>
2023-07-29 01:18:15 +0200

    added profile domain model

---
+++ Shaky_Easy_User/Domain/Profile.cs
@@ -0,0 +1,14 @@
+﻿namespace Shaky_Easy_User.Domain
+{
+    public class Profile
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public Profile(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
