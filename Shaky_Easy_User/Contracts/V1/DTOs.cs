416c8b642937a4e1c5413634240d91ec067d4a0e
Farid <faridbusiness@bk.ru>
2023-07-28 16:34:15 +0200

    added dtos

---
+++ Shaky_Easy_User/Contracts/V1/DTOs.cs
@@ -0,0 +1,23 @@
+﻿namespace Shaky_Easy_User.Contracts.V1
+{
+    public class DTOs
+    {
+        #region Profile
+
+        #region responses
+        public record GetProfileResponse(Guid Id, string name);
+
+
+        #endregion
+
+        #region requests
+        
+        public record CreateProfileRequest(string name);
+
+
+        #endregion
+
+
+        #endregion
+    }
+}
